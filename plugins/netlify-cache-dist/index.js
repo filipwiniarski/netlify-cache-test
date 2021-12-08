0d0f478ca1510358d613e928c7dc4f8014d22061
Filip Winiarski <filipwiniarski@gmail.com>
2021-12-08 13:42:56 +0100

    add console.logs to the cache plugin

--- plugins/netlify-cache-dist/index.js
+++ plugins/netlify-cache-dist/index.js
@@ -3,9 +3,15 @@ const fs = require('fs');
 
 module.exports = {
   async onPreBuild({utils}) {
+    const files = await utils.cache.list({depth: 10})
+    console.log('Cached files', files)
+
     await utils.cache.restore('dist');
   },
   async onPostBuild({utils}) {
     await utils.cache.save('dist');
+
+    const files = await utils.cache.list({depth: 10})
+    console.log('Cached files', files)
   },
 };
